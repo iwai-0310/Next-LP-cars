6fb819942925932238e670d353f418b49bf675af
simpleton <chazesingh@gmail.com>
2024-03-03 20:23:53 +0530

    adds images to public

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -9,11 +9,11 @@ const CustomButton = ({title,containerStyles,handleClick}: CustomButtonProps) =>
     <button
     disabled={false}
     type={"button"}
-    className={`custom-btn`}
-    onClick={()=>{}}
+    className={`custom-btn ${containerStyles}`}
+    onClick={handleClick}
     >
         {/* add button title here */}
-        <span className={`flex-1`}>title</span>
+        <span className={`flex-1`}>{title}</span>
 
     </button>
     
