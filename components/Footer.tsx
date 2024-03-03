70c3e69868689e0348c3167ba9249520f1421366
simpleton <chazesingh@gmail.com>
2024-03-03 23:57:59 +0530

    add privacy,Terms and copyright at footer bottom

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,10 +24,12 @@ const Footer = () => {
         </div>
         {/* In second row  add usefull links to the app/routes and socials*/}
         <div className="footer__links">
+            {/* map the footerlinks data as lists */}
           {footerLinks.map((link) => (
             <div key={link.title} className="footer__link">
                 <h3 className="font-bold">{link.title}</h3>
                 {link.links.map((items)=>(
+                    // map the array inside of items on links
                     <Link key={items.title}
                     href={items.url}
                     className="text-gray-500"
@@ -36,9 +38,23 @@ const Footer = () => {
                     </Link>
                 ))}
             </div>
+
           ))}
         </div>
-      </div>
+        </div>
+        {/* At end add Terms of use and policy rights */}
+        <div className="flex justify-between items-center flex-wrap mt-10
+        border-gray-100 sm:px-16 px-6 py-10">
+            <p>@2024 carhub all rights reserved</p>
+            <div className="footer__copyrights-link">
+                <Link href='/'
+                className="text-gray-500">Privay Policy</Link>
+                <Link href='/'
+                className="text-gray-500">Terms of Use</Link>
+            </div>
+            
+        </div>
+      
     </footer>
   );
 };
