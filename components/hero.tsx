c8e8756b864c48fe6874aea30ae53a53aca1706c
simpleton <chazesingh@gmail.com>
2024-03-03 16:19:50 +0530

    add title and subtitle to the hero-section

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,7 +3,18 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='heor'>
+      <div className="flex-1 pt-36 padding-x">
+        {/* add title here */}
+        <h1 className="hero__title">
+          Find, book or rent a car - in a blink !
+        </h1>
+        {/* add subtitle here */}
+        <p className='hero__subtitle'>
+          Streamline your car rental experience with our effortles booking process
+        </p>
+      </div>
+    </div>
   )
 }
 
