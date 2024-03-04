1f2de7ab169f7afe7bd255f52fd961bf92190331
simpleton <chazesingh@gmail.com>
2024-03-05 00:59:27 +0530

    add rent price and image to the car card component

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,12 +3,16 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { CarProps } from "@/types";
 import CustomButton from "./CustomButton";
+import { calculateCarRent } from "@/utils";
 
 interface CarCardProps {
   car: CarProps;
 }
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
+
+//   access the calculate Rent
+const carRent=calculateCarRent(city_mpg,year);
   return (
     <div className="car-card group">
       <div className="car-card__content">
@@ -17,11 +21,33 @@ const CarCard = ({ car }: CarCardProps) => {
           {make} {model}
         </h2>
       </div>
-        <p>
-            <span>
-                Car Rent. ...
+        <p className="flex mt-6 text-[32px]">
+            <span className="self-start text-[14px] font-semibold">
+                $
             </span>
+            {carRent}
+            <span className="self-end text-[14px] font-medium">
+                /day
+            </span>
+            
         </p>
+        {/* add the image element here */}
+        <div className="relative w-full h-40 my-3 object-contain">
+            <Image src="/hero.png"  alt="car-image"
+            fill priority
+            className="object-contain"/>
+        </div>
+
+        {/* add milegage and other details here */}
+        <div className="relative flex w-full mt-2">
+            <div className="flex group-hover:invisible w-full justify-between
+            text-gray">
+                <div className="flex flex-col items-center justify-center gap-2">
+                    <Image src="/steering-wheel.svg" width={20} height={20}
+                    alt="steering wheel icon"/>
+                </div>
+            </div>
+        </div>
     </div>
   );
 };
