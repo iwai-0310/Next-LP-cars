106ec3556e31361975025726c48c9190d2ff77ad
simpleton <chazesingh@gmail.com>
2024-03-04 16:45:56 +0530

    add Combobox button and input to the searhcManfucturers section

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+'use client'
+import React,{useState} from 'react';
+import Image from 'next/image'
+import { Combobox,Transition } from '@headlessui/react';
 import { SearchManufacturerProps } from '@/types'; 
 
 const SearchManufacturer = ({manufacturer,setManufacturer}:SearchManufacturerProps) => {
   return (
-    <div>SearchManufacturer</div>
+    <div className="search-manufacture">
+        {/* add combobox here */}
+        <Combobox>
+            <div className="relative w-full"> 
+            <Combobox.Button className="absolute top-[14px]">
+                {/* add logo image here */}
+                <Image src='/car-logo.svg' width={20} height={20} className='ml-4'
+                alt='car-logo iamge'/>
+            </Combobox.Button>
+            {/* next to the button add combobox input */}
+            <Combobox.Input className='serach-manufacturer__input'
+            placeholder='volkswagen' displayValue={(manufacture:string)=>
+            manufacture}
+            onChange={}
+            />
+
+            </div>
+        </Combobox>
+    </div>
   )
 }
 
