172a1879a2fa309cc7fd10cbcf173f1fbec8ce8b
simpleton <chazesingh@gmail.com>
2024-03-04 22:35:50 +0530

    modify the utils/index to return all cars as json data

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,16 @@
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '02d88feb0amshda279a27b4d160ap15905ejsn4d80de94d00f',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
-};
+export async function fetchCars() {
+  const headers = {
+    "X-RapidAPI-Key": "02d88feb0amshda279a27b4d160ap15905ejsn4d80de94d00f",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
+    {
+      headers: headers,
+    }
+  );
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+  const result = await response.json();
+
+  return result;
 }
