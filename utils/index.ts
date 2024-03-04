4772cdbe9a7a8e9de7fd545d6aff12e62f72e323
simpleton <chazesingh@gmail.com>
2024-03-04 22:30:44 +0530

    add fetch call instead of earlier axios version

--- utils/index.ts
+++ utils/index.ts
@@ -1,18 +1,16 @@
-const axios = require('axios');
-
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
 const options = {
-  method: 'GET',
-  url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
-  params: {model: 'corolla'},
-  headers: {
-    'X-RapidAPI-Key': '02d88feb0amshda279a27b4d160ap15905ejsn4d80de94d00f',
-    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-  }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '02d88feb0amshda279a27b4d160ap15905ejsn4d80de94d00f',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
 };
 
 try {
-	const response = await axios.request(options);
-	console.log(response.data);
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
 } catch (error) {
 	console.error(error);
 }
