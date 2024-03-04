01eb26adf9477a2d36a7362b73126474f988657d
simpleton <chazesingh@gmail.com>
2024-03-05 00:03:35 +0530

    access the props interface and destructure it in car card compoennt

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,13 +1,29 @@
-'use client'
-import React,{useState} from 'react';
-import Image from 'next/image';
-import { CarProps } from '@/types';
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import { CarProps } from "@/types";
+import CustomButton from "./CustomButton";
 
-
-const CarCard = (car):CarProps => {
-  return (
-    <div>CarCard</div>
-  )
+interface CarCardProps {
+  car: CarProps;
 }
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        {/* add heading for card name and model */}
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+        <p>
+            <span>
+                Car Rent. ...
+            </span>
+        </p>
+    </div>
+  );
+};
 
-export default CarCard
+export default CarCard;
