5539f295da6bb0ee3b9e7140a049eca3c38280b6
simpleton <chazesingh@gmail.com>
2024-03-06 16:14:23 +0530

    add second searchbar with icon for model to the searchBar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,23 @@
 'use client'
 import React,{useState} from 'react'
 import { SearchManufacturer } from './';
+import Image from 'next/image';
+
+const SearchButton=({otherClasses}:{otherClasses:string})=>(
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image src="/magnifying-glass.svg"
+    alt="magnifing glass icon" width={40} height={40}
+    className="object-contain"
+    />
+  </button>
+)
 
 const SearchBar = () => {
 
     //add state to handle manufacturer
     const [manufacturer,setManufacturer] =useState('');
+    //add state to handle model
+    const [model,setModel]=useState("");
     // create function -handleSearch
     const handleSearch=()=>{
 
@@ -19,6 +31,21 @@ const SearchBar = () => {
                 manufacturer={manufacturer}
                 setManufacturer={setManufacturer}
                 />
+                <SearchButton otherClasses="sm:hidden" />
+            </div>
+            <div className='searchbar__item'>
+              <Image src="/model-icon.png"
+              width={25}
+              height={25}
+              className='absolute w-[20px] h-[20px] ml-4'
+              alt="car model icon"/>
+              <input type="text" 
+              name="model"
+              value={model}
+              onChange={(e)=> setModel(e.target.value)}
+              placeholder="Tiguan"
+              className='searchbar__input'
+              />
             </div>
     </form>
   )
