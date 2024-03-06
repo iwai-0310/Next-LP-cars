46e0881ea39233e08b3329d853f0d610ca621c19
simpleton <chazesingh@gmail.com>
2024-03-07 01:26:54 +0530

    path redefine for hero comp

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import {  SearchBar, CustomFIlter,CarCard,ShowMore } from "@/components";
 import Image from "next/image";
 import { fetchCars } from "@/utils";
 import { fuels, yearsOfProduction } from "@/constants";
-import Hero from "@/components/Hero";
+import Hero from "../components/Hero";
 
 export default async function Home({searchParams}) {
   // get all cars data from the server
