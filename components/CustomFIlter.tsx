15776b19685b071887bc1667f6ace2c29a28b11b
simpleton <chazesingh@gmail.com>
2024-03-06 23:18:44 +0530

    text- dropddown for fueland year works

--- components/CustomFIlter.tsx
+++ components/CustomFIlter.tsx
@@ -6,7 +6,7 @@ import {Listbox,Transition} from '@headlessui/react';
 import { CustomFilterProps } from '@/types';
 
 
-const CustomFIlter = ({title,options}:CustomFilterProps) => {
+const CustomFIlter = ({title,options}:CustomFilterProps)=>{
   //add state to handle selected value
   const [selected,setSelected]=useState(options[0]);
   return (
@@ -17,12 +17,37 @@ const CustomFIlter = ({title,options}:CustomFilterProps) => {
       >
         <div className="relative w-fit z-10">
           <Listbox.Button
-          className="custom-filter__btn"
-          >
+          className="custom-filter__btn">
             <span className='block truncate'>{selected.title}</span>
             <Image src="/chevron-up-down.svg"
-            width={20} height={20} alt='up-down svg'/>
+            width={20} height={20} alt='up-down svg'
+            className='ml-4 object-contain'/>
           </Listbox.Button>
+          {/* add transtitons here */}
+          <Transition as={Fragment}
+          leave="transition ease-in duration-300"
+          leaveFrom="opactity-100"
+          leaveTo="opacity-0">
+
+            {/* add listbox options here */}
+            <Listbox.Options
+            className="custom-filter__options">
+              {options.map((option)=>(
+                <Listbox.Option key={option.title} value={option}
+                className={({active})=>`relative cursor-default select-none py-2 px-4
+                ${active ? 'bg-primary-blue text-white': 'text-pink-900'}
+                `}>
+                  {({selected})=>(
+                    <span className={`block truncate
+                    ${selected? 'font-medium':'font-normal'}
+                    `}>
+                      {option.title}
+                    </span>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
         </div>
       </Listbox>
     </div>
