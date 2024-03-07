01c7f7e49b635282ca98b2696794a7bfcc43f828
simpleton <chazesingh@gmail.com>
2024-03-07 14:45:18 +0530

    add key to carCard mapping

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default async function Home({searchParams}:any) {
           <section>
             <div className="home__cars-wrapper">
               {allCars?.map((car) => (
-                <CarCard car={car} />
+                <CarCard key={car.id} car={car} />
               ))}
             </div>
             {/* add show more here */}
